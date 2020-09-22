a6181ded8c355052712bbfd9050c5f5a09222cae
joooscha <joscha.loos@pm.me>
2020-09-22 16:14:17 +0200

    update urls background

---
+++ src/events.rs
@@ -0,0 +1,60 @@
+use std::io;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+use termion::{
+    event::Key,
+    input::TermRead,
+};
+
+pub enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+#[allow(dead_code)]
+pub struct Events {
+    rx: mpsc::Receiver<Event<Key>>,
+    input_handle: thread::JoinHandle<()>,
+    tick_handle: thread::JoinHandle<()>,
+}
+
+impl Events {
+    pub fn new() -> Events {
+        let (tx, rx) = mpsc::channel();
+        let input_handle = {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let stdin = io::stdin();
+                for event in stdin.keys() {
+                    match event {
+                        Ok(key) => {
+                            if let Err(_) = tx.send(Event::Input(key)) {
+                                return;
+                            }
+                        }
+                        Err(_) => {}
+                    }
+                }
+            })
+        };
+        let tick_handle = {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let tx = tx.clone();
+                loop {
+                    tx.send(Event::Tick).unwrap();
+                    thread::sleep(Duration::from_millis(250));
+                }
+            })
+        };
+        Events {
+            rx,
+            input_handle,
+            tick_handle,
+        }
+    }
+    pub fn next(&self) -> Result<Event<Key>, mpsc::RecvError> {
+        self.rx.recv()
+    }
+}
