4232993b9e0a53fb02d7a8a42a1919ba3e618ee8
joooscha <joscha.loos@pm.me>
2020-09-23 12:12:56 +0200

    change standart dateformat to rtc3339 to make sort by time easier

--- fetch_data/target/debug/deps/fetch_data-fe7a9b16dabfd161.d
+++
@@ -1,9 +0,0 @@
-/home/joscha/MEGA/programming/rust/tyt/fetch_data/target/debug/deps/fetch_data-fe7a9b16dabfd161.rmeta: src/lib.rs src/fetch_data.rs src/structs.rs
-
-/home/joscha/MEGA/programming/rust/tyt/fetch_data/target/debug/deps/libfetch_data-fe7a9b16dabfd161.rlib: src/lib.rs src/fetch_data.rs src/structs.rs
-
-/home/joscha/MEGA/programming/rust/tyt/fetch_data/target/debug/deps/fetch_data-fe7a9b16dabfd161.d: src/lib.rs src/fetch_data.rs src/structs.rs
-
-src/lib.rs:
-src/fetch_data.rs:
-src/structs.rs:
