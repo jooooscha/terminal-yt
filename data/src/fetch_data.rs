c0cce964b386d01ba2c1c79d093d9043569c8c13
joooscha <joscha.loos@pm.me>
2021-03-05 10:05:44 +0100

    * restructure; + add some first tests

--- data/src/fetch_data.rs
+++ data/src/fetch_data.rs
@@ -80,14 +80,29 @@ impl Date {
     }
 }
 
+// url file video type
 #[derive(Deserialize, Serialize)]
-struct Videos {
-    videos: Vec<Video>,
+struct UrlFile {
+    videos: Vec<UrlFileChannel>,
+    /* custom_channel: Vec<UrlFileCustomChannel>, */
+}
+
+// url file video type
+#[derive(Clone, Deserialize, Serialize)]
+struct UrlFileChannel {
+    url: String,
+    feed_type: FeedType,
+    #[serde(default = "empty_string")]
+    name: String,
+    #[serde(default = "date_always")]
+    update_on: Vec<Date>,
+    #[serde(default = "empty_string")]
+    tag: String,
 }
 
 #[derive(Clone, Deserialize, Serialize)]
-struct Video {
-    url: String,
+struct UrlFileCustomChannel {
+    url: Vec<String>,
     feed_type: FeedType,
     #[serde(default = "empty_string")]
     name: String,
@@ -107,7 +122,7 @@ fn empty_string() -> String {
 
 
 // impl UrlFile {
-impl Videos {
+impl UrlFile {
     fn len(&self) -> usize {
         self.videos.len()
     }
@@ -118,9 +133,7 @@ pub fn fetch_new_videos(sender: Sender<String>) -> ChannelList {
 
     let today = Local::now().weekday();
 
-    let client = Client::builder().build().ok().unwrap();
-
-    let urls = read_urls_file();
+    let url_file_content = read_urls_file();
 
     let history: ChannelList = match read_history() {
         Some(content) => content,
@@ -130,128 +143,47 @@ pub fn fetch_new_videos(sender: Sender<String>) -> ChannelList {
     let mut channel_list = ChannelList::new();
 
     let worker_num = 4;
-    let jobs_num = urls.len();
+    let jobs_num = url_file_content.len();
     let pool = ThreadPool::new(worker_num);
 
     let (tx, rx) = channel();
 
-    for item in urls.videos {
-        /* if !item.update_on.iter().any(|w| w.eq_to(&today)) {
-         *     jobs_num -= 1;
-         *     continue
-         * } */
-
+    for item in url_file_content.videos {
         let url = item.url.clone();
 
         let tx = tx.clone();
         let sender = sender.clone();
         let history = history.clone();
-        let client = client.clone();
 
         pool.execute(move || {
-            let mut fetched_channel: Option<Channel> = None;
+
+            let mut channel: Option<Channel>;
 
             if item.update_on.iter().any(|w| w.eq_to(&today)) {
                 sender.send(format!("fetching... {}", url.clone())).unwrap();
 
-                let response = match client.get(&url).send() {
-                    Ok(r) => match r.text() {
-                        Ok(e) => Some(e),
-                        Err(_) => None,
-                    },
-                    Err(_) => {
-                        /* notify_user(format!("could not GET url: {}", e)); */
-                        None
-                    }
-                };
-
-
-                if let Some(body) = response {
-                    match item.feed_type {
-                        FeedType::Atom => {
-                            let atom_feed: atom::Feed = match from_str(&body) {
-                                Ok(feed) => feed,
-                                Err(e) => {
-                                    notify_user(&format!("could not parse atom feed: {}", e));
-                                    tx.send(None).unwrap();
-                                    return
-                                }
-                            };
-                            fetched_channel = Some(atom_feed.to_internal_channel());
-                        },
-                        FeedType::Rss => {
-                            let rss_feed: rss::Feed = match from_str(&body) {
-                                Ok(feed) => feed,
-                                Err(e) => {
-                                    notify_user(&format!("could not parse rss feed: {}", e));
-                                    tx.send(None).unwrap();
-                                    return
-                                }
-                            };
-                            fetched_channel = Some(rss_feed.to_internal_channel());
-                        }
+                channel = match download_channel_updates(&url, &history.channels, item) {
+                    Ok(channel) => Some(channel),
+                    Err(err_text) => {
+                        notify_user(&format!("Could not update channel: {}", &err_text));
+                        get_channel_from_history(&url, history.channels)
                     }
                 }
+
+            } else {
+                channel = get_channel_from_history(&url, history.channels);
+            };
+
+            if channel.is_some() {
+                let mut ch = channel.unwrap();
+                ch.videos.sort_by_key(|video| video.pub_date.clone());
+                ch.videos.reverse();
+                channel = Some(ch);
+
             }
 
-            let mut channel = Channel::new_with_url(&url);
-
-            match fetched_channel {
-                Some(content) => {
-                    // prefer name from url file
-                    channel.name = if item.name == String::new() {
-                        content.name
-                    } else {
-                        item.name
-                    };
-                    channel.tag = item.tag;
-
-                    for h in history.channels.iter() {
-                        // match channel links
-                        if h.link == channel.link && h.name == channel.name {
-                            // copy old video elements
-                            
-                            channel.videos = h.videos.clone();
-
-                            break
-                        }
-                    }
-
-                    for mut vid in content.videos.into_iter() {
-                        if !channel.videos.iter().any(|video| video.link == vid.link) {
-                            vid.new = true;
-                            channel.videos.push(vid);
-                        }
-                    }
-
-                },
-                None => {
-                    let mut found = false;
-                    for h in history.channels.iter() {
-                        // match channel links
-                        if h.link == channel.link {
-                            // copy old video elements
-
-                            channel.name = h.name.clone();
-                            /* channel.link = h.link.clone(); */
-                            channel.videos = h.videos.clone();
-
-                            found = true;
-                            break
-                        }
-                    }
-                    if !found {
-                        tx.send(None).unwrap();
-                        return
-                    }
-                }
-            }
-
-            channel.videos.sort_by_key(|video| video.pub_date.clone());
-            channel.videos.reverse();
-
-            tx.send(Some(channel)).unwrap();
-        });
+            tx.send(channel).unwrap(); // channel is type Option
+        }); // end pool
     }
     for chan_opt in rx.iter().take(jobs_num) {
         match chan_opt {
@@ -266,7 +198,7 @@ pub fn fetch_new_videos(sender: Sender<String>) -> ChannelList {
 }
 
 // fn read_urls_file() -> UrlFile {
-fn read_urls_file() -> Videos {
+fn read_urls_file() -> UrlFile {
     let mut path = home_dir().unwrap();
     path.push(URLS_FILE_PATH);
 
@@ -274,12 +206,12 @@ fn read_urls_file() -> Videos {
         Ok(mut file) => {
             let mut reader = String::new();
             file.read_to_string(&mut reader).unwrap();
-            let urls: Vec<Video> = match serde_yaml::from_str(&reader) {
+            let urls: Vec<UrlFileChannel> = match serde_yaml::from_str(&reader) {
                 Ok(file) => file,
                 Err(e) => panic!("could nor parse yaml url-file: {}", e),
             };
 
-            let urls = Videos {
+            let urls = UrlFile {
                 videos: urls,
             };
 
@@ -287,12 +219,163 @@ fn read_urls_file() -> Videos {
         }
         Err(_) => {
             let mut file = File::create(path).unwrap();
-            let videos: Vec<Video> = Vec::new();
+            let videos: Vec<UrlFileChannel> = Vec::new();
             let string = serde_yaml::to_string(&videos).unwrap();
             match file.write_all(string.as_bytes()) {
                 Ok(_) => read_urls_file(),
                 Err(e) => panic!("{}", e),
             }
         }
+    }
+}
+
+fn download_channel_updates(url: &String, history_channels: &Vec<Channel>, url_file_entry: UrlFileChannel) -> Result<Channel, String> {
+    let feed = match fetch_url(&url) {
+        Ok(text) => text,
+        Err(e) => {
+            return Err(format!("Could not GET url: {}", e))
+        }
+    };
+
+    let fetched_channel = match parse_feed_to_channel(&feed) {
+        Ok(channel) => channel,
+        Err(err_text) => {
+            return Err(format!("Could not parse: {}", err_text))
+        }
+    };
+
+    Ok(update_existing_channel(&url, url_file_entry, fetched_channel, &history_channels))
+}
+
+fn fetch_url(url: &String) -> Result<String, reqwest::Error> {
+    let client = Client::builder().build().ok().unwrap();
+
+    match client.get(url).send() {
+        Ok(r) => return r.text(),
+        Err(e) => return Err(e),
+    }
+}
+
+fn parse_feed_to_channel(body: &String) -> Result<Channel, String> {
+
+    // try to parse as atom
+    match from_str::<atom::Feed>(body) {
+        Ok(feed) => return Ok(feed.to_internal_channel()),
+        Err(_) => (),
+    }
+
+    // try to parse as rss
+    match from_str::<rss::Feed>(body) {
+        Ok(feed) => return Ok(feed.to_internal_channel()),
+        Err(_) => (),
+    }
+
+    Err(String::from("Could not parse feed"))
+}
+
+fn update_existing_channel(url: &String, url_file_entry: UrlFileChannel, channel_updates: Channel, history_channels: &Vec<Channel>) -> Channel {
+
+    // create template
+    let mut channel = Channel::new_with_url(&url);
+
+    // set name
+    channel.name = if url_file_entry.name == String::new() {
+        channel_updates.name
+    } else {
+        url_file_entry.name
+    };
+
+    // set tag
+    channel.tag = url_file_entry.tag;
+
+    // match with history item
+    for h in history_channels.iter() {
+        // match channel links
+        if h.link == channel.link && h.name == channel.name {
+
+            // copy old video elements
+            channel.videos = h.videos.clone();
+
+            break
+        }
+    }
+
+    for mut vid in channel_updates.videos.into_iter() {
+        if !channel.videos.iter().any(|video| video.link == vid.link) {
+            vid.new = true;
+            channel.videos.push(vid);
+        }
+    }
+
+    channel
+}
+
+fn get_channel_from_history(url: &String, history_channels: Vec<Channel>) -> Option<Channel> {
+    // create template
+
+    for h in history_channels.iter() {
+
+        // match channel links
+        if &h.link == url {
+
+            let mut channel = Channel::new_with_url(url);
+
+            // copy old video elements
+            channel.name = h.name.clone();
+
+            /* channel.link = h.link.clone(); */
+            channel.videos = h.videos.clone();
+
+            channel.tag = h.tag.clone();
+
+            return Some(channel)
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn parser_test_err() {
+       let output = parse_feed_to_channel(&String::new());
+
+       assert!(output.is_err());
+    }
+
+    #[test]
+    fn parser_test_ok() {
+       let string = "
+           <rss><channel><title>TITLE</title><link>http://example.com</link><description>DESCRIPTION</description><ttl>123</ttl>
+           <item>
+                <title>VIDEO TITLE</title>
+                <link>VIDEO LINK</link>
+                <description>VIDEO DESCRIPTION</description>
+                <guid isPermaLink=\"false\">123</guid>
+                <pubDate>Tue, 02 Mar 2021 18:55:52 UT</pubDate>
+                <category>CATEGORY</category>
+           </item>
+           </channel></rss>";
+
+       let output = parse_feed_to_channel(&String::from(string));
+
+       assert!(output.is_ok());
+    }
+
+    #[test]
+    fn get_channel_from_history_test() {
+        let url = String::from("URL");
+        let mut channel = Channel::new();
+        channel.link = url.clone();
+
+        let mut history_channels = Vec::new();
+        history_channels.push(channel);
+
+        let channel = get_channel_from_history(&url, history_channels);
+
+        assert!(channel.is_some());
     }
 }
