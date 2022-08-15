713dc0ba50364fbcee3eae2fe2f2a14d7d8f4efd
Joscha Loos <j.loos@posteo.net>
2022-08-16 00:35:55 +0200

    build: nix and lock files

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
-  inputs.easy.url = "github:jooooscha/easy-flake";
+  # inputs.easy.url = "github:jooooscha/easy-flake";
+  inputs.easy.url = "path:/home/joscha/main/programming/nix/easy-flake";
 
   outputs = { easy, ...}:
-    easy.rust {
+    easy.rust.env {
       ssl = true;
       root = ./.;
       name = "tyt";
