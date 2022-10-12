dcb3a4807be6e76e1243a4b4ff720d6aa7048dc7
Joscha Loos <j.loos@posteo.net>
2022-10-12 17:39:07 +0200

    flake.nix && flake.lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  # inputs.easy.url = "github:jooooscha/easy-flake";
-  inputs.easy.url = "path:/home/joscha/main/programming/nix/easy-flake";
+  inputs.easy.url = "github:jooooscha/easy-flake";
+  # inputs.easy.url = "path:/home/joscha/main/programming/nix/easy-flake";
 
   outputs = { easy, ...}:
     easy.rust.env {
