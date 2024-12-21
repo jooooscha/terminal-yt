cd8501d7aa0d8ce29f8d0040a4ce5ed299dad087
Joscha Loos <j.loos@posteo.net>
2024-12-21 12:26:10 +0100

    replace 'easy' with 'buildRustPackage'

--- flake.nix
+++ flake.nix
@@ -1,11 +1,33 @@
 {
-  inputs.easy.url = "github:jooooscha/easy-flake";
-  # inputs.easy.url = "path:/home/joscha/main/programming/nix/easy-flake";
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs";
+  };
 
-  outputs = { easy, ...}:
-    easy.rust.x86_64-linux.env {
-      ssl = true;
-      root = ./.;
-      name = "tyt";
+  outputs = { self, nixpkgs }:
+    let
+
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs { inherit system; };
+
+    in {
+
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "tyt";
+        version = "2.0.5";
+        src = pkgs.lib.cleanSource ./.;
+
+        cargoHash = "sha256-ClrNmvnXAyco3ltGPflzi5q/zmNC24V4mwBhDs9SRGw=";
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          openssl
+        ];
+
+        meta = {};
+      };
     };
 }
